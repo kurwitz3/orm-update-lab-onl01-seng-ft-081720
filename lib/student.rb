cade054ef059c37b8765fc575c2109e463496146
Richard Kurwitz <kurwitz3@aol.com>
2020-09-17 20:45:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,12 +32,11 @@ end
    if self.id
      self.update
    else
-   sql = <<-SQL
-    INSERT INTO students (name, grade)
-    VALUES (?, ?)
-   SQL
- 
-   DB[:conn].execute(sql,self.name, self.grade)
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql,self.name, self.grade)
  
    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
  end
@@ -50,11 +49,10 @@ end
  
  def self.find_by_name(name)
    sql = <<-SQL
-   SELECT * FROM students WHERE name = ? LIMIT 1 
+    SELECT * FROM students WHERE name = ? LIMIT 1 
    SQL
-  DB[:conn].execute(sql, name).map do |row|
+   DB[:conn].execute(sql, name).map do |row|
      self.new_from_db(row)
-   
  end.first
 end
 
@@ -63,15 +61,13 @@ end
    name = array[1]
    grade = array[2]
    self.new(id, name, grade)
-  
  end
  
  
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
- 
- end 
+  end 
 end
   
   
